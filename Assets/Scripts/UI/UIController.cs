66525bb023addfa3425336c0b5c0cd2569630a2a
Max <alterra.br@gmail.com>
2020-09-16 15:00:49 +0700

    Update UI logic. Add UI controls for new noise

--- Assets/Scripts/UI/UIController.cs
+++ Assets/Scripts/UI/UIController.cs
@@ -18,6 +18,10 @@ public class UIController : MonoBehaviour
     public Slider IsoSlider;
     public TextMeshProUGUI IsoText;
 
+    public Slider OctavesSlider;
+    public TextMeshProUGUI OctavesSliderText;
+
+    public Toggle DefaultPerlinNoise;
     public Toggle ClassicNoise;
     public Toggle PerlinNoise;
     public Toggle SimplexNoise;
@@ -42,19 +46,43 @@ public class UIController : MonoBehaviour
 
         Player.OnMazeSolved += ShowWinScreen;
     }
+    public void Update()
+    {
+        // Updating UI texts for sliders value
+        ZoomText.text = (ZoomSlider.value / 10).ToString("0.0");
+        SmoothText.text = (SmoothSlider.value / 10).ToString("0.0");
+        MultText.text = MultSlider.value.ToString();
+        IsoText.text = (IsoSlider.value / 10).ToString("0.0");
+        OctavesSliderText.text = OctavesSlider.value.ToString();
+
+        //if (DefaultPerlinNoise.isOn)
+        //{
+        //    ClassicNoise.isOn = false;
+        //    PerlinNoise.isOn = false;
+        //    SimplexNoise.isOn = false;
+        //    Use3DNoise.isOn = false;
+
+        //    ClassicNoise.enabled = !ClassicNoise.enabled;
+        //    PerlinNoise.enabled = !PerlinNoise.enabled;
+        //    SimplexNoise.enabled = !SimplexNoise.enabled;
+        //    Use3DNoise.enabled = !Use3DNoise.enabled;
+        //}
+    }
 
     public void Play()
     {
         GenerateScreen.SetActive(true);
 
-        NoiseParameters.Scale = ZoomSlider.value;
-        NoiseParameters.Smooth = SmoothSlider.value;
+        NoiseParameters.Scale = ZoomSlider.value / 10;
+        NoiseParameters.Smooth = SmoothSlider.value / 10;
         NoiseParameters.Mult = (int)MultSlider.value;
-        NoiseParameters.IsoLevel = IsoSlider.value;
+        NoiseParameters.IsoLevel = IsoSlider.value / 10;
+        NoiseParameters.Octaves = (int)OctavesSlider.value;
 
         NoiseParameters.ClassicNoise = ClassicNoise.isOn;
         NoiseParameters.PerlinNoise = PerlinNoise.isOn;
         NoiseParameters.SimplexNoise = SimplexNoise.isOn;
+        NoiseParameters.DefaultPerlinNoise = DefaultPerlinNoise.isOn;
 
         NoiseParameters.Use3DNoise = Use3DNoise.isOn;
 
@@ -73,15 +101,6 @@ public class UIController : MonoBehaviour
         WinScreen.SetActive(false);
         GenerateMenu.SetActive(true);
         MazeInfoScreen.SetActive(false);
-    }
-
-    public void Update()
-    {
-        // Updating UI texts for sliders value
-        ZoomText.text = ZoomSlider.value.ToString("0.0");
-        SmoothText.text = SmoothSlider.value.ToString("0.0");
-        MultText.text = MultSlider.value.ToString();
-        IsoText.text = IsoSlider.value.ToString("0.0");
     }
 
     private void HideLoader()
@@ -103,7 +122,8 @@ public class UIController : MonoBehaviour
             $"Scale: {ZoomSlider.value}\n" +
             $"Smooth: {SmoothSlider.value}\n" +
             $"Mult: {MultSlider.value}\n" +
-            $"Iso level: {IsoSlider.value}\n\n" +
+            $"Iso level: {IsoSlider.value}\n" +
+            $"Octaves: {OctavesSlider.value}\n\n" +
             $"Used noises: \n" +
             $"{(ClassicNoise.isOn ? "Classic\n" : "")}" +
             $"{(PerlinNoise.isOn ? "Perlin\n" : "")}" +
